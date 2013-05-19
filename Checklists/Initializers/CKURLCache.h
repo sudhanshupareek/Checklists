0b7752d9bff9f71e49e80f325d6a01f82adbde70
Tim Shadel <tim@shadelsoftware.com>
2013-05-19 15:04:38 -0600

    Add URL cache

---
+++ Checklists/Initializers/CKURLCache.h
@@ -0,0 +1,14 @@
+//
+//  CKURLCache.h
+//
+//  Created by Tim Shadel on 5/18/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CKURLCache : NSObject
+
+@property (nonatomic, readonly) NSString *urlCachePath;
+
+@end
---
+++ Checklists/Initializers/CKURLCache.m
@@ -0,0 +1,53 @@
+//
+//  CKURLCache.m
+//
+//  Created by Tim Shadel on 5/18/13.
+//
+//
+
+#import "CKURLCache.h"
+
+static CKURLCache *autoloadedSingletonController;
+
+@interface CKURLCache ()
+@property (nonatomic, strong) NSString *urlCachePath;
+@end
+
+@implementation CKURLCache
+
+
++ (void)load {
+    autoloadedSingletonController = [CKURLCache new];
+}
+
+
+- (id)init
+{
+    self = [super init];
+    if (self != nil) {
+        // Finish Launching
+        [[NSNotificationCenter defaultCenter] addObserver:self
+                                                 selector:@selector(configureURLCacheOnLaunch:)
+                                                     name:UIApplicationDidFinishLaunchingNotification
+                                                   object:[UIApplication sharedApplication]];
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+    [super dealloc];
+}
+
+- (void)configureURLCacheOnLaunch:(NSNotification *)launchNotification
+{
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES);
+    self.urlCachePath = [(NSString *)[paths objectAtIndex:0] stringByAppendingPathComponent: @"URLCache"];
+    NSURLCache *URLCache = [[NSURLCache alloc] initWithMemoryCapacity:4 * 1024 * 1024
+                                                         diskCapacity:20 * 1024 * 1024
+                                                             diskPath:self.urlCachePath];
+    [NSURLCache setSharedURLCache:URLCache];
+}
+
+@end
